6a96bcdda7e858ef20b0dc8259080f6ceda0a596
Alexandre <alexandrecarlos2@gmai.com>
2023-09-14 22:11:50 -0300

    iniciando testes

--- backend/ride/src/main.ts
+++ backend/ride/src/main.ts
@@ -10,15 +10,18 @@ app.post("/calc", function (req, res) {
     for (const mov of req.body) {
 
         mov.ds = new Date(mov.ds);
+        console.log(mov.ds);
+        console.log(mov.ds.getHours());
+        console.log(mov.ds.getDay());
 
         if (mov.dist != null && mov.dist != undefined && typeof mov.dist === "number" && mov.dist > 0)  
         {
-
             if (mov.ds != null && mov.ds != undefined && mov.ds instanceof Date && mov.ds.toString() !== "Invalid Date") 
             {
 
                 //overnight
-                if (mov.ds.getHours() >= 22 || mov.ds.getHours() <= 6) {
+                if (mov.ds.getHours() >= 22 || mov.ds.getHours() <= 6) 
+                {
 
                     //not sunday
                     if (mov.ds.getDay() !== 0)  {
