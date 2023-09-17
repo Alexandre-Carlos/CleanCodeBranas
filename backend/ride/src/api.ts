e730afe5a0756c2592e86e4152179f3845112225
Alexandre <alexandrecarlos2@gmai.com>
2023-09-17 13:36:55 -0300

    code smells - extrair constante e teste da api para exceções

--- backend/ride/src/api.ts
+++ backend/ride/src/api.ts
@@ -1,18 +1,31 @@
 // @ts-nocheck
 import express from 'express';
 import { calculate } from './RideCalculator';
+import Ride from './Ride';
 const app = express();
 app.use(express.json());
 
 //Meio de transmissão ou driver
 app.post("/calculate_ride", function (req, res) {
     //início do negócio
-    for (const segment of req.body.segments) {
-        segment.date = new Date(segment.date);
+    //procedural - diretamente ligado ao domínio
+    // for (const segment of req.body.segments) {
+    //     segment.date = new Date(segment.date);
+    // }
+    // const price = calculate(req.body.segments);
+
+    //forma orientada a objeto
+    try {
+        const ride = new Ride();
+        for (const segment of req.body.segments) {
+            ride.addSegment(segment.distance, new Date(segment.date));
+        }
+        const price = ride.calculate();
+        //fim do negócio
+        res.json({ price });
+    } catch (e) {
+        res.status(422).send(e.message);
     }
-    const price = calculate(req.body.segments);
-    //fim do negócio
-    res.json({ price });
 });
 
 app.listen(3000);
--- backend/ride/test/api.test.ts
+++ backend/ride/test/api.test.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
+axios.defaults.validateStatus = function(){
+    return true;
+};
 
 test("Deve fazer o cáculo do preço de uma corrida durante o dia", async function (){
     // given, when, then
@@ -14,6 +17,18 @@ test("Deve fazer o cáculo do preço de uma corrida durante o dia", async functi
     const output = response.data;
     //then
     expect(output.price).toBe(21);
+});
+
+test("Se a distância for inválida deve lançar um erro", async function (){
+    const input = { 
+        segments: [
+            { distance: -10, date: "2021-03-01T10:00:00"}
+        ]
+    };
+    const response = await axios.post("http://localhost:3000/calculate_ride", input);
+    expect(response.status).toBe(422);
+    const output = response.data;
+    expect(output).toBe("Invalid distance");
 });
 
 
