ea86bac55c82f09c506031155f266007ef2d57fe
Alexandre <alexandrecarlos2@gmai.com>
2023-09-17 15:01:43 -0300

    code smells - finalizando aula 1

--- backend/ride/src/api.ts
+++ backend/ride/src/api.ts
@@ -7,14 +7,6 @@ app.use(express.json());
 
 //Meio de transmissão ou driver
 app.post("/calculate_ride", function (req, res) {
-    //início do negócio
-    //procedural - diretamente ligado ao domínio
-    // for (const segment of req.body.segments) {
-    //     segment.date = new Date(segment.date);
-    // }
-    // const price = calculate(req.body.segments);
-
-    //forma orientada a objeto
     try {
         const ride = new Ride();
         for (const segment of req.body.segments) {
